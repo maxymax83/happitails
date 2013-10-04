e47a628dad12cf3d39beb84514c93225f253911a
Shaun Garland <shaungarland@shaun-garlands-macbook-pro.local>
2013-10-04 10:55:41 +1000

    here is the new client file

--- client.rb
+++ client.rb
@@ -2,6 +2,20 @@ class Client
   attr_accessor :name, :num_children, :age, :num_pets
 
   def initialize
+    puts "Firstly, please tell me your name"
+    @name = gets.chomp.capitalize
+    puts "Thanks #{name}! Do you have any children? If so, how many?"
+    @num_children.to_i
+    puts "And how old are you #{name}?"
+    @age = gets.chomp.to_i
+    puts "So young! Do you already have any pets? If so, how many?"
+    @num_pets = gets.chomp.to_i
   end
 
 end
+
+
+A client should have a name.
+A client should have a number of children.
+A client should have an age.
+A client should have a number of pets.
